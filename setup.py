9408bd5deca6d328466f4622315726711a63f5c5
Balaji S. Srinivasan <balajis@21.co>
2015-08-07 17:50:56 -0700

    setup.py: removed two1.crypto from required packages

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- Mode: Python -*-
 """two1
 
 This tool uses the official PyPa packaging and click recommendations:
@@ -35,28 +36,20 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Internet',
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
     keywords='bitcoin blockchain client server',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['two1', 'two1.commands'],
+    packages=['two1', 'two1.commands', 'two1.bitcoin', 'two1.mining'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['click'],
+    install_requires=['click', 'cython'],
+    ext_modules=extensions,
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -78,7 +71,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('peers', ['data/default-peers.json'])],
+    # data_files=[('peers', ['data/default-peers.json'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
