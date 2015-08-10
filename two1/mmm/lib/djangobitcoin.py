8432f2f72ab4c83766264436a84c64edcc11a716
Andr3wHur5t <Andr3wHur5t@live.com>
2015-08-10 16:24:39 -0700

    Examples: Updated authentication to conform to 402 spec.

--- two1/mmm/lib/djangobitcoin.py
+++ two1/mmm/lib/djangobitcoin.py
@@ -20,7 +20,7 @@ payment_required_body_text = "Payment Required"
 # This is the applications exception handler, we will write our exception
 # HTTP responses here.
 def PaymentRequiredExceptionHandler(exc, context):
-    # Get the default exeption response from the rest framework.
+    # Get the default exception response from the rest framework.
     response = exception_handler(exc, context)
 
     # Override with custom status code, if needed
@@ -42,19 +42,23 @@ def shouldBeConfiguredFor402(exc, response):
     return hasattr(exc, 'paymentInfo') and exc.paymentInfo['price'] is not 0
 
 def configureFor402(response, price, address):
-    # update statuscode
+    # update status code
     response.status_code = status.HTTP_402_PAYMENT_REQUIRED
 
     # Set payment headers
-    response["bitcoin-address"] = address
-    response["price"] = price
+    response["Bitcoin-Address"] = address
+    response["Price"] = price
+
+    # TODO: Set quote persistence headers
+    # response["Price-Token-Expiration"]
+    # response["Price-Token"]
 
     return response
 
 
 class PaymentRequiredAuthentication(authentication.BaseAuthentication):
 
-    # Entrypoint for verification
+    # Entry point for verification
     def authenticate(self, request):
         # Check the transaction and get the state
         tx = self.getTransactionFor(request)
@@ -74,14 +78,14 @@ class PaymentRequiredAuthentication(authentication.BaseAuthentication):
             raise self.paymentRequiredException(
                 request, payment_required_body_text)
 
-    # This can be overriden by a subclass to support dynamic prices.
+    # This can be overridden by a subclass to support dynamic prices.
     # NOTE: this method should be 100% deterministic or transaction
     # verification will fail.
     def getQuoteFor(self, request):
         # Use Default Price
         return getattr(settings, 'BITSERV_DEFAULT_PRICE')
 
-    # This can be overriden by a subclass to support generated addresses
+    # This can be overridden by a subclass to support generated addresses
     # NOTE: this method should be 100% deterministic or transaction
     # verification will fail.
     def getAddressFor(self, request):
@@ -104,29 +108,19 @@ class PaymentRequiredAuthentication(authentication.BaseAuthentication):
         # 1. Header
         # This will only use the Header eventually. All other accepts are for
         # testing
-        if request.META.get("HTTP_TXID"):
-            return request.META.get("HTTP_TXID")
+        if request.META.get("HTTP_BITCOIN-TRANSACTION"):
+            return request.META.get("HTTP_BITCOIN-TRANSACTION")
 
-        # 2. Body
-        elif "txId" in request.POST:
-            return request.POST["txId"]
-
-        # 3. Query
-        elif "txId" in request.GET:
-            return request.GET["txId"]
-
-        elif "tx" in request.POST:
-            return request.POST["tx"]
-
-        # 3. Query
+        # 2. Query
         elif "tx" in request.GET:
             return request.GET["tx"]
-
         else:
             return None
 
     def paymentRequiredException(self, request, message):
         exe = exceptions.AuthenticationFailed(payment_required_body_text)
+
+        # TODO: Validate outputs...
         exe.paymentInfo = {
             'price': self.getQuoteFor(request),
             'address': self.getAddressFor(request),
