e8787aee81e5af55996b77a8e34f12ee5044fb86
veerbhan <veerbhan@21.co>
2015-08-15 16:16:27 -0700

    UX/Copywriting: Factored out ux strings into their own class

---
+++ two1/uxstring.py
@@ -0,0 +1,14 @@
+
+
+class UxString:
+	#account creation
+	creating_account = "Creating 21.co account. Username %s."
+	payout_address   = "Payout address: %s."
+	account_failed   = "Failed to create 21.co account."
+
+
+	class Error:
+		#network errors
+		connection = "Cannot connect to %s."
+		timeout    = "Connection to %s timed out."
+
