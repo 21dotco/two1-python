90ba8ad266d80dab2e7ec7f274fa5cc0a09863fd
veerbhan <veerbhan@21.co>
2015-08-21 02:41:18 -0700

    cmd/status: Scafolding for 21 status

--- two1/commands/status.py
+++ two1/commands/status.py
@@ -2,20 +2,72 @@ import click
 from two1.config import pass_config
 from two1.debug import dlog
 
+import time
+import datetime
+from random import randint
+
 @click.command()
-@click.option('--private',
-              is_flag=True,
-              default=False,
-              help='Show private keys.')
 @pass_config
-def status(config, private):
+def status(config):
     """View earned Bitcoin and configuration"""
 
     foo = config.fmt()
-    click.echo("\nConfig\n------\n" + foo)
-    config.log("Invoked config.log")
-    click.echo("config.verbose = %s" % config.verbose)
-    config.vlog("Invoked config.vlog")
-    
-    #comment this until the wallet is stable
-    #click.echo("Bitcoin Balance: %g Satoshi" % config.wallet.confirmed_balance())    
+    config.log(
+      '''
+21.co Account: {}'''
+         .format(config.username)
+      )
+    time.sleep(1.0)
+
+    b_seed = ord(config.username[0])
+    balance_c = int(b_seed * 10000 + datetime.datetime.now().minute * 8000)
+    balance_u = int(b_seed * 10000 + (datetime.datetime.now().minute+1) * 8000)
+
+    config.log('''
+Wallet
+    Balance (confirmed)   : {} Satoshi
+    Balance (unconfirmed) : {} Satoshi
+    Payout Address        : {}'''
+      .format(balance_c,balance_u,config.bitcoin_address)
+      )
+
+    mining_p = int(time.time() / 9.0)
+    shares = int(time.time() / 1.0)
+
+    config.log('''
+Mining Proceeds
+    Mining Status         : Live
+    Mining Proceeds       : {}
+    Shares Sent           : {}'''
+      .format(mining_p,shares)
+      )
+
+    status_endpoints(config)
+
+def status_endpoints(config):
+
+    time.sleep(1.0)
+
+    headers =  \
+           ("URL","Price","MMM?","Description","# Requests","Earnings")
+    header_format = "{:^15}|{:^10}|{:^5}|{:^25}|{:^10}|{:^12}"
+    data_format   = "{:<15.15}|{:>10}|{:^5}|{:<25.25}|{:>10}|{:>12}"
+    dundee1_format = "{:_^16}{:_^11}{:_^6}{:_^26}{:_^11}{:_^13}"
+    dundee2_format = "{:_^15}|{:_^10}|{:_^5}|{:_^25}|{:_^10}|{:_^12}"
+
+    endpoint_data = [
+              ("misc/en2cn",1000,"No","English to Chinese",27,27000),
+              ("dice/bet"  ,1000,"Yes","Satoshi Dice",127,-127000),
+              ("numpy/eigen",10000,"Yes","Eigen Values",7,70000),
+              ("blackjack/",1000,"No","Blackjack",17,17000),
+              ("misc/cn2en",2000,"No","Chinese to English",2,4000),
+    ]
+    dundee_data = ["" for n in range(len(headers))]
+    config.log("\nMMM Endpoints")
+    config.log(dundee1_format.format(*dundee_data))
+    config.log(header_format.format(*headers))
+    config.log(dundee2_format.format(*dundee_data))
+    for edata in endpoint_data:
+      config.log(data_format.format(*edata))
+    config.log(dundee2_format.format(*dundee_data))
+    config.log("")
