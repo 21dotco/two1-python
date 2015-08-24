7686a8f89335360efdde7f4096bca7bd9454ce2e
Chris Cassano <glitch0@gmail.com>
2015-08-24 15:08:20 -0700

    cmd/search: scaffolding for 21 search

--- two1/commands/search.py
+++ two1/commands/search.py
@@ -1,9 +1,16 @@
 import click
 from two1.config import pass_config
+from two1.config import TWO1_HOST
+from two1.mining import rest_client
+from two1.bitcoin.crypto import PrivateKey
 
 @click.command()
 @pass_config
-def search(config, query="translation", reverse=False):
+@click.argument('query', nargs=1)
+def search(config, query, reverse=False):
     "Search the Many Machine Market (MMM)"
     print("Searching the MMM for %s" % query)
+    # create a rest client with a dummy private key because we are using all unsigned get requests
+    mining_rest_client = rest_client.MiningRestClient(PrivateKey.from_random(), TWO1_HOST)
+    mining_rest_client.get_and_print_all_search_pages(query)
     return query
--- two1/commands/sell.py
+++ two1/commands/sell.py
@@ -1,10 +1,12 @@
 import json
 import signal
+from subprocess import Popen, PIPE
+
+from re import split
 import click
-from django.conf import settings as django_settings
-from two1.djangobitcoin.djangobitcoin.settings import ENDPOINTS_FILE
 import os
 from two1.config import pass_config
+from two1.djangobitcoin.djangobitcoin.settings import ENDPOINTS_FILE
 import two1.djangobitcoin.djangobitcoin as dj_bt
 
 
@@ -21,29 +23,65 @@ def sell(config, items, port=8000, builtin=False):
     return
 
 
+def try_config_django():
+    try:
+        from django.conf import settings as django_settings
+        django_settings.configure()
+        return True
+    except Exception:
+        click.echo('Error importing Django. It is installed?')
+        return False
+
+
 def find_endpoint(endpoint, package_name):
-    package = __import__(package_name, fromlist=['urls'])
-    urls = getattr(package, 'urls').urlpatterns
-    if endpoint in map(lambda u: u.regex.pattern, urls):
-        return package_name
+    try:
+        package = __import__(package_name, fromlist=['urls'])
+        urls = getattr(package, 'urls').urlpatterns
+        if endpoint in map(lambda u: u.regex.pattern, urls):
+            return package_name
+    except Exception as e:
+        click.echo('Error importing {0}: {1}'.format(package_name, e))
+        return None
+
+
+def find_process_by_attribute(attribute):
+    process_list = []
+    sub_proc_ps = Popen(['ps', 'auxw', ], stdout=PIPE)
+    sub_proc = Popen(['grep', attribute, ], stdin=sub_proc_ps.stdout, stdout=PIPE)
+    sub_proc_ps.stdout.close()
+    # Discard the first line (ps aux header)
+    sub_proc.stdout.readline()
+    for line in sub_proc.stdout:
+        # The separator for splitting is 'variable number of spaces'
+        proc_info = split(" *", line.decode('utf-8').strip())
+        process_list.append(proc_info[1])  # pid
+    if process_list:
+        return min(process_list)
 
 
 def find_process():
-    # TODO find process, possibly with psutil
     # gunicorn process is started gunicorn --pythonpath two1/djangobitcoin djangobitcoin.wsgi --bind 127.0.0.1:8000
-    # python process is started python manage.py runserver 127.0.0.1:8000
-    pass
+    # built-in django server is started python manage.py runserver 127.0.0.1:8000
+    gunicorn_pid = find_process_by_attribute('djangobitcoin.wsgi')
+    if gunicorn_pid:
+        return gunicorn_pid, True
+    django_svr_pid = find_process_by_attribute('runserver')
+    if django_svr_pid:
+        return django_svr_pid, False
+    return None, None
 
 
 def sell_item(item, port):
+    if not try_config_django():
+        return
     endpoints_path = os.path.join(dj_bt.__path__[0], ENDPOINTS_FILE)
     ep_json = json.load(open(endpoints_path))
-    django_settings.configure()
     package = find_endpoint(item, 'two1.djangobitcoin.misc') \
               or find_endpoint(item, 'two1.djangobitcoin.scipy_aas') \
               or find_endpoint(item, 'two1.djangobitcoin.static_serve')
     if not package:
         click.echo('Endpoint {0} not found'.format(item))
+        return
     click.echo('Selling %s on http://127.0.0.1:%d/' % (item, port))
     package_element = next((x for x in ep_json if x['package'] == package), None)
     if not package_element:
@@ -56,19 +94,31 @@ def sell_item(item, port):
     package_element['urls'].append(item)
     with open(endpoints_path, 'w') as outfile:
         json.dump(ep_json, outfile, indent=2)
+    click.echo('Endpoints configuration updated.')
     pid, is_gunicorn = find_process()
     if is_gunicorn:
-        os.kill(pid, signal.SIGHUP)
+        os.kill(int(pid), signal.SIGHUP)
+        click.echo('Server restarted')
     elif pid:
-        click.echo('Endpoints configuration updated. Restart the server')
+        click.echo('Restart the server')
+
+
+def try_url_imports(package_name):
+    try:
+        package = __import__(package_name, fromlist=['urls'])
+        return package.urls.urlpatterns
+    except Exception as e:
+        click.echo('Error importing {0}: {1}'.format(package_name, e))
+        return []
 
 
 def show_builtins():
-    click.echo("BUILTINS")
-    django_settings.configure()
-    from two1.djangobitcoin.misc import urls as misc_urls
-    from two1.djangobitcoin.scipy_aas import urls as scipy_urls
-    from two1.djangobitcoin.static_serve import urls as static_serve_urls
-    urls = misc_urls.urlpatterns + scipy_urls.urlpatterns + static_serve_urls.urlpatterns
+    click.echo("\nBUILTINS\n")
+    if not try_config_django():
+        return
+    misc_urls = try_url_imports('two1.djangobitcoin.misc')
+    scipy_urls = try_url_imports('two1.djangobitcoin.scipy_aas')
+    static_urls = try_url_imports('two1.djangobitcoin.static_serve')
+    urls = misc_urls + scipy_urls + static_urls
     for u in urls:
         click.echo(u.regex.pattern)
