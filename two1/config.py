7016c2dcde0bb4ff754ca29f26ea19b52e6ecf45
Chris Cassano <glitch0@gmail.com>
2015-08-24 15:08:20 -0700

    cmd/search: scaffolding for 21 search

--- two1/config.py
+++ two1/config.py
@@ -9,23 +9,25 @@ from path import path
 from two1.wallet import electrumWallet
 from two1.debug import dlog
 from two1.uxstring import UxString
-import pkg_resources 
+import pkg_resources
 
 TWO1_CONFIG_FILE = path("~/.two1/two1.json")
 TWO1_PURCHASES_FILE = path("~/.two1/purchases.json")
 TWO1_HOST = "http://twentyone-devel-1d3c.herokuapp.com"
-#TWO1_HOST = "http://127.0.0.1:8000"
+# TWO1_HOST = "http://127.0.0.1:8000"
 
 try:
     TWO1_VERSION = pkg_resources.require("two1")[0].version
 except:
     TWO1_VERSION = "undefined"
-    
+
 
 '''Primary use case for the following class is the singleton that holds
-   all the state & config data required to run commands and subcommands 
+   all the state & config data required to run commands and subcommands
    for two1 app
 '''
+
+
 class Config(object):
 
     def __init__(self, config_file=TWO1_CONFIG_FILE, config=None):
@@ -33,19 +35,19 @@ class Config(object):
         self.dir = self.file.parent
         self.defaults = {}
         self.load()
-        #override config variables
-        for k,v in config:
-            self.defaults[k]=v
+        # override config variables
+        for k, v in config:
+            self.defaults[k] = v
 
         if self.verbose:
             self.vlog("Applied manual config.")
-            for k,v in config:
-                self.vlog("  {}={}".format(k,v))
+            for k, v in config:
+                self.vlog("  {}={}".format(k, v))
 
-        #add wallet object
-        self.wallet = electrumWallet.ElectrumWallet()          
+        # add wallet object
+        self.wallet = electrumWallet.ElectrumWallet()
 
-        #create an empty purchases file if it does not exist
+        # create an empty purchases file if it does not exist
         self.purchases_file = path(TWO1_PURCHASES_FILE).expand().abspath()
         if self.purchases_file.exists() and self.purchases_file.isfile():
             pass
@@ -53,7 +55,7 @@ class Config(object):
             with open(self.purchases_file, mode='w', encoding='utf-8') as f:
                 pass
 
-    #pulls attributes from the self.defaults dict
+    # pulls attributes from the self.defaults dict
     def __getattr__(self, name):
         if name in self.defaults:
             return self.defaults[name]
@@ -70,8 +72,8 @@ class Config(object):
             self.file.rmdir()
         with open(self.file + ".tmp", mode="w", encoding='utf-8') as fh:
             json.dump(self.defaults, fh, indent=2, sort_keys=True)
-        #move file if successfully written
-        os.rename(self.file+".tmp",self.file)
+        # move file if successfully written
+        os.rename(self.file + ".tmp", self.file)
         return self
 
     def load(self):
@@ -85,42 +87,41 @@ class Config(object):
                 self.defaults = {}
 
         defaults = dict(username=getpass.getuser(),
-                                 sellprice=10000,
-                                 contact="two1@21.co",
-                                 stdout=".two1/two1.stdout",
-                                 stderr=".two1/two1.stderr",
-                                 bitin=".bitcoin/wallet.dat",
-                                 bitout=".bitcoin/wallet.dat",
-                                 sortby="price",
-                                 maxspend=20000,
-                                 verbose=False,
-                                 mining_auth_pubkey=None)
-
+                        sellprice=10000,
+                        contact="two1@21.co",
+                        stdout=".two1/two1.stdout",
+                        stderr=".two1/two1.stderr",
+                        bitin=".bitcoin/wallet.dat",
+                        bitout=".bitcoin/wallet.dat",
+                        sortby="price",
+                        maxspend=20000,
+                        verbose=False,
+                        mining_auth_pubkey=None)
 
         save_config = False
-        for key,default_value in defaults.items():
+        for key, default_value in defaults.items():
             if key not in self.defaults:
-                self.defaults[key]=default_value
-                save_config=True
+                self.defaults[key] = default_value
+                save_config = True
 
         if save_config:
             self.save()
 
         return self
 
-    def update_key(self,key,value):
+    def update_key(self, key, value):
         self.defaults[key] = value
-        #might be better to switch to local sqlite for persisting 
-        #the config
-        #self.save()
+        # might be better to switch to local sqlite for persisting
+        # the config
+        # self.save()
 
     # kwargs is styling parameters
-    def log(self, msg, *args,**kwargs):
+    def log(self, msg, *args, **kwargs):
         """Logs a message to stderr."""
         if args:
             msg %= args
         if len(kwargs) > 0:
-            out = click.style(msg,**kwargs)
+            out = click.style(msg, **kwargs)
         else:
             out = msg
         click.echo(out, file=sys.stderr)
@@ -130,14 +131,14 @@ class Config(object):
         if self.verbose:
             self.log(msg, *args)
 
-    def log_purchase(self,**kwargs):
-        #simple logging to file
-        #this can be replaced with pickle/sqlite
+    def log_purchase(self, **kwargs):
+        # simple logging to file
+        # this can be replaced with pickle/sqlite
         with open(self.purchases_file, mode='a', encoding='utf-8') as pjson:
-            pjson.write(json.dumps(kwargs)+"\n")
+            pjson.write(json.dumps(kwargs) + "\n")
 
     def get_purchases(self):
-        #read all right now. TODO: read the most recent ones only
+        # read all right now. TODO: read the most recent ones only
         try:
             with open(self.purchases_file, mode='r', encoding='utf-8') as pjson:
                 content = pjson.readlines()
@@ -145,9 +146,6 @@ class Config(object):
         except:
             dlog("Error: Could not load purchases.")
             return []
-
-        
-
 
     def fmt(self):
         pairs = []
--- two1/mining/rest_client.py
+++ two1/mining/rest_client.py
@@ -3,112 +3,135 @@
 # Create a/c
 # Set payout addresses etc.
 from two1.bitcoin.crypto import PrivateKey
-from two1.bitcoin.crypto import PublicKey
-from two1.bitcoin.crypto import Signature
 from two1.bitcoin.utils import bytes_to_str, address_to_key_hash
 import base64
 import requests
 import json
 from urllib.parse import urljoin
 
+
 class MiningAuth(object):
-	def __init__(self,private_key):
-		self.private_key=private_key
-		self.public_key=private_key.public_key
 
-	def create(self):
-		pass
+    def __init__(self, private_key):
+        self.private_key = private_key
+        self.public_key = private_key.public_key
 
-	def load(self):
-		pass
+    def create(self):
+        pass
 
-	def sign(self,message):
-		if type(message) == type(''):
-			utf8 = message.encode('utf-8')
-		else:
-			raise ValueError
-		signature = self.private_key.sign(utf8).to_base64()
-		return signature
+    def load(self):
+        pass
 
-#		compressed_bytes = base64.b64encode(self.public_key.compressed_bytes)
-##		signature = signature.to_base64()
+    def sign(self, message):
+        if isinstance(message, str):
+            utf8 = message.encode('utf-8')
+        else:
+            raise ValueError
+        signature = self.private_key.sign(utf8).to_base64()
+        return signature
+        # compressed_bytes = base64.b64encode(self.public_key.compressed_bytes)
+        # signature = signature.to_base64()
 
-#		print("Things",compressed_bytes,signature,utf8)
+        # print("Things",compressed_bytes,signature,utf8)
 
-#		signature = Signature.from_base64(signature)
-#		pubk = PublicKey.from_base64(compressed_bytes)
-#		print("VERIFICATION RESULT: %g " % pubk.verify(utf8, signature))
+        # signature = Signature.from_base64(signature)
+        # pubk = PublicKey.from_base64(compressed_bytes)
+        # print("VERIFICATION RESULT: %g " % pubk.verify(utf8, signature))
 
 
 class MiningRestClient(object):
 
-	def __init__(self,private_key,server_url,version="v0"):
-		self.auth = MiningAuth(private_key)
-		self.server_url = server_url
-		self.version = version
+    def __init__(self, private_key, server_url, version="v0"):
+        self.auth = MiningAuth(private_key)
+        self.server_url = server_url
+        self.version = version
 
-	def _request(self,signed,method,path,**kwargs):
-		url = self.server_url+path+"/"
-		headers={}
-		if "data" in kwargs:
-			headers["Content-Type"]="application/json"
-			data = kwargs["data"]
-		else:
-			data = ""
-		if signed:
-			sig = self.auth.sign(data)
-			headers["Authorization"]=sig.decode()
-		if len(headers) == 0:
-			headers = None
-		#print("Request: " + str(method)+ " " + str(url)  + " " + str(headers)  + " " + str(kwargs["data"]))
-		result = requests.request(method,
-								url,
-								headers=headers,
-								**kwargs)
-		#print("Result: %s %s " % (result,result.text))
-		return result
+    def _request(self, signed, method, path, **kwargs):
+        url = self.server_url + path + "/"
+        headers = {}
+        if "data" in kwargs:
+            headers["Content-Type"] = "application/json"
+            data = kwargs["data"]
+        else:
+            data = ""
+        if signed:
+            sig = self.auth.sign(data)
+            headers["Authorization"] = sig.decode()
+        if len(headers) == 0:
+            headers = None
+        # print("Request: " + str(method)+ " " + str(url)  + " " + str(headers)  + " " + str(kwargs["data"]))
+        result = requests.request(method,
+                                  url,
+                                  headers=headers,
+                                  **kwargs)
+        # print("Result: %s %s " % (result,result.text))
+        return result
 
-	#POST /v0/mining/account
-	def account_post(self,username,payout_address):
-		method = "POST"
-		path = "/v0/mining/account/" + username
-		body = {
-				"payout_address": payout_address,
-				"public_key": base64.b64encode(self.auth.public_key.compressed_bytes).decode(),
-				}
-		data=json.dumps(body)
-		return self._request(True,method,
-						path,
-						data=json.dumps(body)
-						);
+    # POST /v0/mining/account
+    def account_post(self, username, payout_address):
+        method = "POST"
+        path = "/v0/mining/account/" + username
+        body = {
+            "payout_address": payout_address,
+            "public_key": base64.b64encode(self.auth.public_key.compressed_bytes).decode(),
+        }
+        data = json.dumps(body)
+        return self._request(True, method,
+                             path,
+                             data=data
+                             )
 
-	#POST /v0/mining/account/payout_address/{username}
-	def account_payout_address_post(self,username,payout_address):
-		method = "POST"
-		path = "/v0/mining/account/payout_address/" + username
-		body = {
-				"payout_address": payout_address,
-				}
-		data = json.dumps(body)
-		return self._request(True,method,
-						path,
-						data=data
-						);
+    # POST /v0/mining/account/payout_address/{username}
+    def account_payout_address_post(self, username, payout_address):
+        method = "POST"
+        path = "/v0/mining/account/payout_address/" + username
+        body = {
+            "payout_address": payout_address,
+        }
+        data = json.dumps(body)
+        return self._request(True, method,
+                             path,
+                             data=data
+                             )
+
+    def _print_search_results_page(self, sells):
+        for sell in sells:
+            print("{} by user {}".format(sell["name"], sell["username"]))
+            print("\tDescription: {}".format(sell["description"]))
+            print("\tRating: {}".format(sell["rating"]))
+            print("\tPrice: {}".format(sell["price"]))
+
+    def get_and_print_all_search_pages(self, query, page_num=1):
+        method = "GET"
+        path = "/mmm/sells/search/"
+        r = self._request(False, method, path, params={"q": query, "page": page_num})
+        if r.status_code == 200:
+            parsed = json.loads(r.content.decode())
+            if len(parsed["data"]) == 0:
+                print('No results found for query "{}"'.format(query))
+                return True
+            else:
+                self._print_search_results_page(parsed["data"])
+                if parsed["pages"]["current_page"] < parsed["pages"]["total_pages"]:
+                    self.get_and_print_all_search_pages(query, page_num=page_num + 1)
+                else:
+                    return True
+        else:
+            print("Error contacting server.  Server response code is {}.  Body is {}".format(
+                r.status_code, r.content.decode()))
+            return False
 
 
-if __name__=="__main__":
-#	pk = PrivateKey.from_random()
-#	m = MiningRestClient(pk,"http://127.0.0.1:8000")
-	host = "http://127.0.0.1:8000"
-	for n in range(100):
-		pk = PrivateKey.from_random()
-		m = MiningRestClient(pk,host)
-		try:
-			m.account_post("testuser11210_"+str(n),"1BHZExCqojqzmFnyyPEcUMWLiWALJ32Zp5")
-			m.account_payout_address_post("testuser11210_"+str(n),"1LuckyP83urTUEJE9YEaVG2ov3EDz3TgQw")
+if __name__ == "__main__":
+    # pk = PrivateKey.from_random()
+    # m = MiningRestClient(pk,"http://127.0.0.1:8000")
+    host = "http://127.0.0.1:8000"
+    for n in range(100):
+        pk = PrivateKey.from_random()
+        m = MiningRestClient(pk, host)
+        try:
+            m.account_post("testuser11210_" + str(n), "1BHZExCqojqzmFnyyPEcUMWLiWALJ32Zp5")
+            m.account_payout_address_post("testuser11210_" + str(n), "1LuckyP83urTUEJE9YEaVG2ov3EDz3TgQw")
 
-		except requests.exceptions.ConnectionError:
-			print("Error: cannot connect to ",host)
-
-
-
+        except requests.exceptions.ConnectionError:
+            print("Error: cannot connect to ", host)
