c5117a3fd504684d3da46f84cb607ad6906333e2
veerbhan <veerbhan@21.co>
2015-08-26 14:46:17 -0700

    wallet: Enable creation of a new wallet (if none exists)

--- two1/uxstring.py
+++ two1/uxstring.py
@@ -2,22 +2,27 @@
 
 class UxString:
 
-	#account creation
+	# account creation
 	creating_account = "Creating 21.co account. Username %s."
 	payout_address   = "Payout address: %s."
 	account_failed   = "Failed to create 21.co account."
 	username_exists  = "Username %s already exists."
 	enter_username   = "Enter Username for the account:"
 
+	# wallet
+	create_wallet = "You do not have a Bitcoin wallet configured. Let's create one. Press any key to continue ...\n"
+	create_wallet_done = "\nWallet successfully created. Press any key to continue..."
+	payout_address = "Setting mining payout address: %s."
+
 	class Error:
-		#network errors
+		# network errors
 		connection = "Error: Cannot connect to %s."
 		timeout    = "Error: Connection to %s timed out."
 
-		#file io errors
+		# file io errors
 		file_load = "Error: Failed to load file %s."
 
-		#wallet errors
+		# wallet errors
 		electrum_missing = "Error: Could not find ElectrumWallet application."
 		electrum_daemon = "Error: Could not start electrum daemon."
 
