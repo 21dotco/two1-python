3cb585309488de76e0d8c4bf4a59a8fe7774daff
Ali Fathalian <ali@21.co>
2015-08-31 16:36:58 -0700

    bug_fix: Fixed the indentation error in uxstring

--- two1/uxstring.py
+++ two1/uxstring.py
@@ -15,14 +15,6 @@ class UxString:
             "We are working to correct this issue."
         non_existing_user = "Error: Username %s does not exist"
 
-	class Error:
-		# network errors
-		connection = "Error: Cannot connect to %s."
-		timeout    = "Error: Connection to %s timed out."
-		# 500 unknown error
-		server_err = "Error: You have experienced a Technical Error. We are working to correct this issue."
-		non_existing_user = "Error: Username %s does not exist"
-
         # wallet errors
         electrum_missing = "Error: Could not find ElectrumWallet application."
         electrum_daemon = "Error: Could not start electrum daemon."
