d70906436db0c55cae32a903ddac5ab9289afba5
Peter Thomas <pjthomas404@gmail.com>
2015-10-07 18:32:24 -0700

    commands/rate.py import fixes

--- two1/commands/rate.py
+++ two1/commands/rate.py
@@ -1,6 +1,12 @@
 import click
 from two1.commands.config import pass_config
 
+from two1.commands.config import pass_config, TWO1_HOST, TWO1_DEV_HOST
+from two1.lib.server.rest_client import TwentyOneRestClient
+from two1.lib.server.machine_auth import MachineAuth
+#from decimal import Decimal, localcontext, ROUND_DOWN
+
+
 @click.command()
 @pass_config
 def rate(config):
