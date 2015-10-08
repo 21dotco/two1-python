3f5eafbe0b3adb1ee6334870b23ee86e500fb90a
21 Team <git@21.co>
2015-10-07 21:42:47 -0700

    lib/bitcoin/script.py: Add functions to extract multisig signature data.

--- two1/lib/bitcoin/tests/test_script.py
+++ two1/lib/bitcoin/tests/test_script.py
@@ -59,11 +59,11 @@ def test_multisig():
 
     serialized_pubkeys = [bytes.fromhex(p) for p in pubkeys]
 
-    ret = Script.build_multisig(2, serialized_pubkeys)
+    ret = Script.build_multisig_redeem(2, serialized_pubkeys)
 
     assert bytes_to_str(bytes(ret['redeemScript'])) == "52410491bba2510912a5bd37da1fb5b1673010e43d2c6d812c514e91bfa9f2eb129e1c183329db55bd868e209aac2fbc02cb33d98fe74bf23f0c235d6126b1d8334f864104865c40293a680cb9c020e7b1e106d8c1916d3cef99aa431a56d253e69256dac09ef122b1a986818a7cb624532f062c1d1f8722084861c5c3291ccffef4ec687441048d2455d2403e08708fc1f556002f1b6cd83f992d085097f9974ab08a28838f07896fbab08f39495e15fa6fad6edbfb1e754e35fa1c7844c41f322a1863d4621353ae"
 
-    assert ret['redeemScript'].is_multisig()
+    assert ret['redeemScript'].is_multisig_redeem()
     
     assert bytes_to_str(bytes(ret['pubKeyScript'])) == "a914f815b036d9bbbce5e9f2a00abd1bf3dc91e9551087"
     hash160 = bytes.fromhex(ret['pubKeyScript'].get_hash160()[2:])
@@ -77,24 +77,35 @@ def test_multisig():
 def test_is_p2pkh():
     s = Script("OP_DUP OP_HASH160 0x68bf827a2fa3b31e53215e5dd19260d21fdf053e OP_EQUALVERIFY OP_CHECKSIG")
     assert s.is_p2pkh()
-    assert not s.is_multisig()
+    assert not s.is_multisig_redeem()
 
     s = Script.build_p2pkh(bytes.fromhex("68bf827a2fa3b31e53215e5dd19260d21fdf053e"))
     assert s.is_p2pkh()
     
     s = Script("OP_ADD OP_HASH160 0x68bf827a2fa3b31e53215e5dd19260d21fdf053e OP_EQUALVERIFY")
     assert not s.is_p2pkh()
-    assert not s.is_multisig()
+    assert not s.is_multisig_redeem()
 
 
 def test_is_p2sh():
     s = Script("OP_HASH160 0x68bf827a2fa3b31e53215e5dd19260d21fdf053e OP_EQUAL")
     assert s.is_p2sh()
-    assert not s.is_multisig()
+    assert not s.is_multisig_redeem()
 
     s = Script.build_p2sh(bytes.fromhex("68bf827a2fa3b31e53215e5dd19260d21fdf053e"))
     assert s.is_p2sh()
 
     s = Script("OP_ADD OP_HASH160 0x68bf827a2fa3b31e53215e5dd19260d21fdf053e OP_EQUALVERIFY")
     assert not s.is_p2sh()
-    assert not s.is_multisig()
+    assert not s.is_multisig_redeem()
+
+
+def test_multisig_sig():
+    sig_script = Script('OP_0 0x30440220762ce7bca626942975bfd5b130ed3470b9f538eb2ac120c2043b445709369628022051d73c80328b543f744aa64b7e9ebefa7ade3e5c716eab4a09b408d2c307ccd701 0x3045022100abf740b58d79cab000f8b0d328c2fff7eb88933971d1b63f8b99e89ca3f2dae602203354770db3cc2623349c87dea7a50cee1f78753141a5052b2d58aeb592bcf50f01 OP_PUSHDATA1 0xc9 0x524104a882d414e478039cd5b52a92ffb13dd5e6bd4515497439dffd691a0f12af9575fa349b5694ed3155b136f09e63975a1700c9f4d4df849323dac06cf3bd6458cd41046ce31db9bdd543e72fe3039a1f1c047dab87037c36a669ff90e28da1848f640de68c2fe913d363a51154a0c62d7adea1b822d05035077418267b1a1379790187410411ffd36c70776538d079fbae117dc38effafb33304af83ce4894589747aee1ef992f63280567f52f5ba870678b4ab4ff6c8ea600bd217870a8b4f1f09f3a8e8353ae')
+
+    assert sig_script.is_multisig_sig()
+
+    r = sig_script.extract_multisig_sig_info()
+    assert len(r['signatures']) == 2
+    assert r['redeem_script']
+    assert isinstance(r['redeem_script'], Script)
