6105d7c841bb3973bc4ab5efa7196bacdb4350c2
21 Team <21team@21.co>
2015-10-09 09:51:01 -0700

    lib/bitcoin/script.py: Add functions to get hash160

--- two1/lib/bitcoin/tests/test_script.py
+++ two1/lib/bitcoin/tests/test_script.py
@@ -64,6 +64,10 @@ def test_multisig():
     assert bytes_to_str(bytes(ret['redeemScript'])) == "52410491bba2510912a5bd37da1fb5b1673010e43d2c6d812c514e91bfa9f2eb129e1c183329db55bd868e209aac2fbc02cb33d98fe74bf23f0c235d6126b1d8334f864104865c40293a680cb9c020e7b1e106d8c1916d3cef99aa431a56d253e69256dac09ef122b1a986818a7cb624532f062c1d1f8722084861c5c3291ccffef4ec687441048d2455d2403e08708fc1f556002f1b6cd83f992d085097f9974ab08a28838f07896fbab08f39495e15fa6fad6edbfb1e754e35fa1c7844c41f322a1863d4621353ae"
 
     assert ret['redeemScript'].is_multisig_redeem()
+
+    # Get the address of the redeem script
+    assert ret['redeemScript'].address().startswith("3")
+    assert ret['redeemScript'].address(True).startswith("2")
     
     assert bytes_to_str(bytes(ret['pubKeyScript'])) == "a914f815b036d9bbbce5e9f2a00abd1bf3dc91e9551087"
     hash160 = bytes.fromhex(ret['pubKeyScript'].get_hash160()[2:])
@@ -78,6 +82,8 @@ def test_is_p2pkh():
     s = Script("OP_DUP OP_HASH160 0x68bf827a2fa3b31e53215e5dd19260d21fdf053e OP_EQUALVERIFY OP_CHECKSIG")
     assert s.is_p2pkh()
     assert not s.is_multisig_redeem()
+
+    assert not s.address()
 
     s = Script.build_p2pkh(bytes.fromhex("68bf827a2fa3b31e53215e5dd19260d21fdf053e"))
     assert s.is_p2pkh()
