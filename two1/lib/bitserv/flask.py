108e949ed8ec7998cf9f709f51cd4e4b9877a62d
Tyler J <tylerj@21.co>
2015-10-26 16:53:29 -0700

    lib/bitserv: add flask decorator for on-chain 402 payments

---
+++ two1/lib/bitserv/flask.py
@@ -0,0 +1,86 @@
+"""Flask bitserv payment library for selling 402 API endpoints."""
+from flask import request
+from functools import wraps
+from werkzeug.exceptions import HTTPException, BadRequest
+from .methods import OnChain
+
+
+class PaymentRequiredException(HTTPException):
+
+    """Payment required exception."""
+
+    code = 402
+
+    def get_body(self, context):
+        """402 response body."""
+        return 'Payment Required'
+
+    def get_headers(self, context):
+        """402 response headers."""
+        payment_headers = self.description
+        return payment_headers
+
+
+class Payment:
+
+    """Class to store merchant settings."""
+
+    def __init__(self, app, db=None, default_price=None, default_address=None):
+        """Configure bitserv settings.
+
+        Args:
+            app (flask.Flask): a flask app to wrap payment handling around.
+        """
+        self.default_price = default_price
+        self.default_address = default_address
+        self.allowed_methods = [OnChain(db)]
+
+    def required(self, price=None, address=None):
+        """API route decorator to request payment for a resource.
+
+        This function stores the resource price in a closure. It will verify
+        the validity of a payment, and allow access to the resource if the
+        payment is successfully accepted.
+
+        Usage:
+            @app.route('/myroute')
+            @payment.required(100, '1MDxJYsp4q4P46RiigaGzrdyi3dsNWCTaR')
+
+        Raises:
+            PaymentRequiredException: HTTP 402 response with payment headers.
+        """
+        def decorator(fn):
+            @wraps(fn)
+            def _fn(*args, **kwargs):
+                # Get headers for initial 402 response
+                payment_headers = {}
+                for method in self.allowed_methods:
+                    payment_headers.update(method.get_402_headers(
+                        price=price or self.default_price,
+                        address=address or self.default_address))
+
+                # Continue to the API view if payment is valid
+                if self.is_valid_payment(request.headers, payment_headers):
+                    return fn(*args, **kwargs)
+                else:
+                    raise PaymentRequiredException(payment_headers)
+            return _fn
+        return decorator
+
+    def is_valid_payment(self, request_headers, payment_headers):
+        """Validate the payment information received in the request headers.
+
+        Args:
+            request_headers (dict): Headers sent by client with their request.
+            payment_headers (dict): Required headers to verify the client's
+                request against.
+        """
+        for method in self.allowed_methods:
+            if method.should_redeem(request_headers):
+                try:
+                    v = method.redeem_payment(request_headers, payment_headers)
+                except Exception as e:
+                    print(str(e))  # TODO better logging for errors
+                    raise BadRequest(str(e))
+                return v
+        return False
---
+++ two1/lib/bitserv/methods.py
@@ -0,0 +1,139 @@
+"""Allowed payment methods."""
+from two1.lib.bitcoin.txn import Transaction
+from two1.lib.blockchain.twentyone_provider import TwentyOneProvider
+from two1.commands.config import TWO1_HOST
+from .onchain_data import OnChainSQLite3
+
+
+class PaymentError(Exception):
+    pass
+
+
+class InsufficientPaymentError(PaymentError):
+    pass
+
+
+class InvalidPaymentParameterError(PaymentError):
+    pass
+
+
+class DuplicatePaymentError(PaymentError):
+    pass
+
+
+class TransactionBroadcastError(PaymentError):
+    pass
+
+
+class PaymentBase:
+
+    """Base class for payment methods."""
+
+    def should_redeem(self, request_headers):
+        """Method for checking if we should use a derived payment method."""
+        return all(h in request_headers.keys() for h in self.payment_headers)
+
+    @property
+    def payment_headers(self):
+        """Derived list of headers to use for payment processing.
+
+        Returns:
+            (list): List of required headers that a client should present
+                in order to redeem a payment of this method.
+                Example: ['Bitcoin-Transaction']
+        """
+        raise NotImplementedError()
+
+    def get_402_headers(self, **kwargs):
+        """Derived dict of headers to return in the initial 402 response.
+
+        Args:
+            (**kwargs):
+        Returns:
+            (dict): Dict of headers that the server uses to inform the client
+                how to remit payment for the resource.
+                Example: {'address': '1MDxJYsp4q4P46RiigaGzrdyi3dsNWCTaR',
+                          'price': 500}
+        """
+        raise NotImplementedError()
+
+    def redeem_payment(self, request_headers, payment_headers):
+        """Derived method for processing and validating payment.
+
+        Args:
+            request_headers (dict): Headers sent by client with their request.
+            payment_headers (dict): Required headers to verify the client's
+                request against.
+        Returns:
+            (boolean): Whether or not the payment is valid.
+        Raises:
+            InsufficientPaymentError: Payment received does not match the
+                resource's price.
+            InvalidPaymentParameterError: Payment was made to an address other
+                than the merchant's provided address.
+            DuplicatePaymentError: Payment has already been used to pay for a
+                resource from the merchant.
+        """
+        raise NotImplementedError()
+
+
+###############################################################################
+
+
+class OnChain(PaymentBase):
+
+    """Making a payment on the bitcoin blockchain."""
+
+    http_payment_data = 'Bitcoin-Transaction'
+    http_402_price = 'Price'
+    http_402_address = 'Bitcoin-Address'
+
+    def __init__(self, db=None):
+        """Initialize payment handling for on-chain payments."""
+        self.db = db
+        if db is None:
+            self.db = OnChainSQLite3()
+
+    @property
+    def payment_headers(self):
+        """List of headers to use for payment processing."""
+        return [OnChain.http_payment_data]
+
+    def get_402_headers(self, **kwargs):
+        """Dict of headers to return in the initial 402 response."""
+        return {OnChain.http_402_price: kwargs['price'],
+                OnChain.http_402_address: kwargs['address']}
+
+    def redeem_payment(self, request_headers, payment_headers):
+        """Validate the transaction and broadcast it to the blockchain."""
+        raw_tx = request_headers[OnChain.http_payment_data]
+        print('Receieved transaction: {}'.format(raw_tx))
+        try:
+            payment_tx = Transaction.from_hex(raw_tx)
+        except:
+            raise InvalidPaymentParameterError('Error: Invalid tx hex.')
+
+        resource_price = payment_headers[OnChain.http_402_price]
+
+        # Find the output with the merchant's address
+        merchant_address = payment_headers[OnChain.http_402_address]
+        payment_index = payment_tx.output_index_for_address(merchant_address)
+        if payment_index is None:
+            raise InvalidPaymentParameterError('Error: Not paid to merchant.')
+
+        # Verify that the payment is made for the correct amount
+        if payment_tx.outputs[payment_index].value != resource_price:
+            raise InsufficientPaymentError('Error: Incorrect payment amount.')
+
+        # Store and verify that we haven't seen this payment before
+        txid, new = self.db.get_or_create(str(payment_tx.hash), resource_price)
+        if not new:
+            raise DuplicatePaymentError('Error: Payment already used.')
+
+        # Broadcast payment
+        try:
+            TwentyOneProvider(TWO1_HOST).broadcast_transaction(raw_tx)
+        except Exception as e:
+            raise TransactionBroadcastError(str(e))
+
+        return True
