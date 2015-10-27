73bcbe69f3b57992903351451e72b639842a36af
Tyler J <tylerj@21.co>
2015-10-27 13:51:30 -0700

    lib/bitserv: test channel close and fix mock blockchain provider
    also clean up assertion syntax

--- two1/lib/bitserv/test_app.py
+++ two1/lib/bitserv/test_app.py
@@ -9,6 +9,7 @@ from two1.lib.bitcoin.script import Script
 from .wallet import Two1WalletWrapper, MockTwo1Wallet
 from .paymentserver import PaymentServer, PaymentServerError
 from .channel_data import DatabaseSQLite3
+from .blockchain import MockBlockchain
 
 
 TEST_DEP_AMOUNT = 100000
@@ -17,6 +18,7 @@ TEST_FEE_AMOUNT = 10000
 cust_wallet = MockTwo1Wallet()
 merch_wallet = MockTwo1Wallet()
 server = PaymentServer(merch_wallet, testnet=True)
+server._blockchain = MockBlockchain()
 
 
 def _create_client_txs():
@@ -56,7 +58,7 @@ def test_discovery():
     test_public_key = codecs.encode(
         merch_wallet._private_key.public_key.compressed_bytes,
         'hex_codec').decode('utf-8')
-    assert(merchant_public_key == test_public_key)
+    assert merchant_public_key == test_public_key
 
 
 def test_initialize_handshake():
@@ -67,7 +69,7 @@ def test_initialize_handshake():
 
     # Initialize the handshake and ensure that it returns sucessfully
     initialized = server.initialize_handshake(refund_tx)
-    assert(initialized)
+    assert initialized
 
     # Test for handshake failure when using the same refund twice
     with pytest.raises(PaymentServerError):
@@ -88,7 +90,7 @@ def test_complete_handshake():
     # Test that handshake completion succeeds
     server.initialize_handshake(refund_tx)
     completed = server.complete_handshake(deposit_txid, deposit_tx)
-    assert(completed)
+    assert completed
 
     # Test that handshake completion fails with a duplicate deposit
     with pytest.raises(PaymentServerError):
@@ -109,7 +111,7 @@ def test_receive_payment():
     server.initialize_handshake(refund_tx)
     server.complete_handshake(deposit_txid, deposit_tx)
     paid = server.receive_payment(deposit_txid, payment_tx)
-    assert(paid)
+    assert paid
 
     # Test that payment receipt fails with a duplicate payment
     with pytest.raises(PaymentServerError):
@@ -123,23 +125,38 @@ def test_redeem_payment():
     deposit_txid = str(deposit_tx.hash)
     payment_txid = str(payment_tx.hash)
 
-    # Test that payment receipt fails when no channel exists
+    # Test that payment redeem fails when no channel exists
     with pytest.raises(PaymentServerError):
         server.redeem(payment_txid)
 
-    # Test that payment receipt succeeds
+    # Test that payment redeem succeeds
     server.initialize_handshake(refund_tx)
     server.complete_handshake(deposit_txid, deposit_tx)
     server.receive_payment(deposit_txid, payment_tx)
-    payment_txid = str(payment_tx.hash)
 
-    amount = server.redeem(payment_txid)
-    assert(amount == TEST_PMT_AMOUNT)
+    amount = server.redeem(str(payment_tx.hash))
+    assert amount == TEST_PMT_AMOUNT
 
-    # Test that payment receipt fails with a duplicate payment
+    # Test that payment redeem fails with a duplicate payment
     with pytest.raises(PaymentServerError):
         server.redeem(payment_txid)
 
-# def test_status_close_channel():
-#     """Test ability to get a channel's status and close it."""
-#     assert(True)
+
+def test_status_close_channel():
+    """Test ability to get a channel's status and close it."""
+    server._db = DatabaseSQLite3(':memory:')
+    deposit_tx, refund_tx, payment_tx = _create_client_txs()
+    deposit_txid = str(deposit_tx.hash)
+
+    # Test that channel close fails when no channel exists
+    with pytest.raises(PaymentServerError):
+        server.close(deposit_txid)
+
+    # Test that channel close succeeds
+    server.initialize_handshake(refund_tx)
+    server.complete_handshake(deposit_txid, deposit_tx)
+    server.receive_payment(deposit_txid, payment_tx)
+    server.redeem(str(payment_tx.hash))
+
+    closed = server.close(deposit_txid)
+    assert closed
