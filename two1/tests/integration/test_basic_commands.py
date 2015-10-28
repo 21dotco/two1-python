de490f7a56890a45e89dfb8c815086620ac54218
Corentin Debains <corentin@21.co>
2015-10-28 16:53:38 -0700

    two1/tests/integration: Moving conftest.py, adding integration opt
    * --integration will now run the integration tests in pytest
    * Moving the conftest from integration to the top to have it picked up

--- two1/tests/integration/test_basic_commands.py
+++ two1/tests/integration/test_basic_commands.py
@@ -1,9 +1,14 @@
 import pytest
 from click.testing import CliRunner
 from two1.cli import main
-from util import random_str
 import pexpect
 
+integration = pytest.mark.skipif(
+    not pytest.config.getoption("--integration"),
+    reason="need --integration option to run"
+)
+
+@integration
 def test_21_mine(cli_runner):
     child = cli_runner.spawn('mine')
     child.expect(pexpect.EOF)
