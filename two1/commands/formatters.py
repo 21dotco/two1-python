d6d5dcc501a436a85762eaa0775b54b6fb34a616
Balaji S. Srinivasan <balajis@21.co>
2015-11-02 06:44:33 -0800

    commands/formatters: Limit search results to 10 by default

--- two1/commands/formatters.py
+++ two1/commands/formatters.py
@@ -5,22 +5,23 @@ from textwrap import wrap
 from tabulate import tabulate
 
 
-def search_formatter(res):
+def search_formatter(res, maxresults=10):
     """custom formatter for search."""
     formatted_search_results = []
     headers = ("No.", "Result")
     search_results = json.loads(res.json()['results'])['d']['results']
     for i, search_result in enumerate(search_results):
-        title = search_result["Title"]
-        url = search_result["Url"]
-        desc = search_result["Description"]
+        if i < maxresults:
+            title = search_result["Title"]
+            url = search_result["Url"]
+            desc = search_result["Description"]
 
-        formatted_search_results.append([i, title])
-        formatted_search_results.append(["", "-----------------"])
-        formatted_search_results.append(["", url])
-        for i, l in enumerate(wrap(desc, 80)):
-            formatted_search_results.append(["", l])
-        formatted_search_results.append(["", ""])
+            formatted_search_results.append([i, title])
+            formatted_search_results.append(["", "-----------------"])
+            formatted_search_results.append(["", url])
+            for k, l in enumerate(wrap(desc, 80)):
+                formatted_search_results.append(["", l])
+            formatted_search_results.append(["", ""])
     return(
         tabulate(
             formatted_search_results,
