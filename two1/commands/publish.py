70b519bdc33ae195329c04a0d0f4c34a0a7a5523
Balaji S. Srinivasan <balajis@21.co>
2015-11-08 16:57:55 -0800

    commands/stubs: Add stubs for search, rate, sell, publish.
    We refer developers to the 21 Developer Community at slack.21.co
    and indicate that these commands are coming soon.

--- two1/commands/publish.py
+++ two1/commands/publish.py
@@ -1,27 +1,13 @@
 import click
-
-from click import ClickException
-from two1.lib.server.analytics import capture_usage
-
-from two1.lib.server.machine_auth import MachineAuth
-from two1.lib.server import login
-from two1.lib.server.rest_client import TwentyOneRestClient
-from two1.commands.config import pass_config
-from two1.commands.config import TWO1_HOST
-
+from two1.lib.util.uxstring import UxString
 
 @click.command()
-@pass_config
-@click.argument('name')
-@click.argument('description')
-@click.argument('price', type=click.INT)
-def publish(config, name, description, price):
-    """Publish your endpoint to the MMM"""
+@click.pass_context
+def publish(config):
+    """Publish a machine-payable endpoint.
+    """
+    click.echo(UxString.publish_stub)
+               
+               
 
-    rest_client = TwentyOneRestClient(TWO1_HOST,
-                                      login.get_auth_key(),
-                                      config.username)
 
-    rest_client.mmm_create_sell(name, description, price)
-    config.log("Published {} endpoint. Price: {}".format(name,price))
-    # return config
--- two1/commands/search.py
+++ two1/commands/search.py
@@ -1,33 +1,9 @@
 import click
-from tabulate import tabulate
-from two1.commands.config import TWO1_HOST
-from two1.lib.server.rest_client import ServerRequestError
-from two1.commands.config import pass_config
-from two1.commands.config import TWO1_API_HOST
-from two1.lib.server.analytics import capture_usage
-from two1.lib.server.rest_client import TwentyOneRestClient
-
-
-def search_lib(query="", order=None, detail=False, silent=False):
-    rest_client = TwentyOneRestClient(TWO1_API_HOST)
-    listings = rest_client.search(query, detail=detail)
-    if order is not None and order in listings[0]._fields:
-        listings = sorted(listings, key=lambda xx: getattr(xx, order))
-    if not silent:
-        click.echo(tabulate(listings, headers="keys", tablefmt="psql"))
-    return listings
-
+from two1.lib.util.uxstring import UxString
 
 @click.command()
-@pass_config
-@click.option('--query', default="", nargs=1,
-              help="Search listings on the Many Machine Market")
-@click.option('--order', default=None,
-              help="Column to order search results")
-@click.option('--detail', default=False, is_flag=True,
-              help="Return detailed results with parameter information")
-@click.option('--silent', default=False, is_flag=True,
-              help="Do not echo results to the command line")
-def search(config, query, order, detail, silent):
-    "Search the Many Machine Market (MMM)."
-    search_lib(query, order, detail, silent)
+@click.pass_context
+def search(config):
+    """Search for a machine-payable endpoint.
+    """
+    click.echo(UxString.search_stub)
