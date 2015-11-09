ff38d4cbf930a9697309c5e66f4a34b4a6c3f33a
Balaji S. Srinivasan <balajis@21.co>
2015-11-08 19:00:27 -0800

    commands/send: Add rudimentary 21 send command.

--- two1/commands/send.py
+++ two1/commands/send.py
@@ -1,48 +1,47 @@
-import decimal
-import subprocess
 import click
-from two1.lib.wallet.cli import send_to
 
-send = send_to
+from two1.lib.util.decorators import json_output
 
-# from two1.lib.wallet.base_wallet import convert_to_btc
-# from two1.lib.util.decorators import json_output
 
-# @click.command("send")
-# @click.argument('address', type=click.STRING)
-# @click.argument('satoshis', type=click.INT)
-# @json_output
-# def send(config, address, satoshis):
-#     """Send the specified address some satoshis.
+@click.command("send")
+@click.argument('address', type=click.STRING)
+@click.argument('satoshis', type=click.INT)
+@json_output
+def send(config, address, satoshis):
+    """Send the specified address some satoshis.
 
-# \b
-# Usage
-# -----
-# Mine bitcoin at 21.co, flush it to the Blockchain, and then send 5000
-# to the Apache Foundation.
-# $ 21 mine
-# $ 21 flush
-# # Wait ~10-20 minutes for flush to complete and block to mine
-# $ 21 send 1BtjAzWGLyAavUkbw3QsyzzNDKdtPXk95D 1000 
-# """
-#     btc = str(convert_to_btc(satoshis))
-#     return subprocess.check_output(["wallet","sendto", address, btc])
-    
-    # w = config.wallet.w
-    # FEES = 0
-    # balance = w.confirmed_balance()
-    # if balance > satoshis + FEES:
-    #     txids = w.send_to(address=address,
-    #                       amount=satoshis)
-    # else:
-    #     click.echo("Insufficient Blockchain balance of %s satoshis.\n"\
-    #                "Cannot send %s satoshis to %s.\n"\
-    #                "Do %s, then %s to increase your Blockchain balance." %
-    #                (balance, satoshis, address,
-    #                 click.style("21 mine", bold=True),
-    #                 click.style("21 flush", bold=True)))
-    #     txids = []
-    # return txids
-
-    
-    
+\b
+Usage
+-----
+Mine bitcoin at 21.co, flush it to the Blockchain, and then send 5000
+to the Apache Foundation.
+$ 21 mine
+$ 21 flush
+# Wait ~10-20 minutes for flush to complete and block to mine
+$ 21 send 1BtjAzWGLyAavUkbw3QsyzzNDKdtPXk95D 1000
+"""
+    w = config.wallet.w
+    FEES = 0  # Ideal: get this from wallet
+    balance = w.confirmed_balance()
+    if balance > satoshis + FEES:
+        txids = w.send_to(address=address,
+                          amount=satoshis)
+        if len(txids) > 0:
+            assert len(txids) == 1, "Unexpected: more than one transaction"
+            txid = txids[0]["txid"]
+            tx = txids[0]["tx"]
+            click.echo("Successfully sent %s satoshis to %s.\n"
+                       "txid: %s\n"
+                       "tx: %s\n"
+                       "To see in the blockchain: "
+                       "https://blockexplorer.com/address/%s\n"
+                       % (satoshis, address, txid, tx, address))
+    else:
+        click.echo("Insufficient Blockchain balance of %s satoshis.\n"
+                   "Cannot send %s satoshis to %s.\n"
+                   "Do %s, then %s to increase your Blockchain balance." %
+                   (balance, satoshis, address,
+                    click.style("21 mine", bold=True),
+                    click.style("21 flush", bold=True)))
+        txids = []
+    return txids
