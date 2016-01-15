dcd0ba58eb0ad8fc907ae624674e779ce398dd30
Tyler J <tylerj@21.co>
2016-01-15 12:28:26 -0800

    examples/bitcoin_auth/authentication: fix acceptable buy price
    - Changes the django merchant server to accept payment from a client,
        even in cases where the client has overpaid. This prevents a bug
        where the merchant doesn't accept the first payment made in a
        payment channel, which may be rounded up to cover the dust limit.

--- two1/examples/bitcoin_auth/authentication.py
+++ two1/examples/bitcoin_auth/authentication.py
@@ -346,7 +346,7 @@ class PaymentServerAuthentication(BaseBitcoinAuthentication):
             # Redeem the transaction in its payment channel
             paid_amount = payment_server.redeem(request.META[payment_header])
             # Verify the amount of the payment against the resource price
-            if paid_amount == int(get_price_for_request(request)):
+            if paid_amount >= int(get_price_for_request(request)):
                 validation = (self.pc_user, paid_amount)
         finally:
             return validation
