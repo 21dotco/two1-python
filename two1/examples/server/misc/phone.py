9e781f5a104462ee17054627b38ccbc1ff9f72b8
Ronak Mehta <ronakmehta@Ronaks-MacBook-Pro.local>
2016-02-03 16:44:56 -0800

    examples: updated price of SMS to be above dust limit

--- two1/examples/server/misc/phone.py
+++ two1/examples/server/misc/phone.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view
 from two1.examples.bitcoin_auth.pricing import api_price
 
 
-@api_price(1000)
+@api_price(3000)
 @api_view(['POST'])
 def send_sms(request):
     """
