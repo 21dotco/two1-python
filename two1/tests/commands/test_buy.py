19c63c459b01605f5992a0cf2403e5b5c63ccd76
Tyler J <tylerj@21.co>
2016-02-16 13:27:38 -0800

    tests/commands/test_buy: add unit tests for `21 buy`
    - two1/tests/mock: add mock classes for monkeypatching external
        function behavior during tests

---
+++ two1/tests/commands/test_buy.py
@@ -0,0 +1,152 @@
+"""Unit tests for `21 buy`."""
+import collections
+
+import click
+import pytest
+
+import two1.tests.mock as mock
+import two1.commands.buy as buy
+import two1.lib.util.uxstring as uxstring
+
+
+def test_get_buy(patch_click, mock_config, patch_bitrequests, patch_rest_client):
+    """Test a standard GET buy."""
+    resource = 'http://127.0.0.1:5000'
+    buy._buy(mock_config, resource)
+
+    assert patch_bitrequests.method == 'get'
+    assert patch_bitrequests.url == resource
+    assert patch_bitrequests.max_price == 10000
+    assert len(patch_bitrequests.headers.keys()) == 0
+    assert patch_bitrequests.data is None
+    assert patch_bitrequests.response.status_code == 200
+    assert patch_bitrequests.response.text == mock.MockBitResponse.SUCCESS_RESPONSE
+    assert patch_bitrequests.response.amount_paid == mock.MockBitResponse.GET_COST
+    assert patch_click.call_count == 2
+    patch_click.assert_any_call(uxstring.UxString.buy_balances.format(patch_bitrequests.response.amount_paid, '21.co', mock.MockTwo1Wallet.BALANCE - mock.MockBitResponse.GET_COST), err=True)
+    patch_click.assert_any_call(mock.MockBitResponse.SUCCESS_RESPONSE, file=None)
+
+
+def test_info_buy(patch_click, mock_config, patch_bitrequests):
+    """Test a information-only buy."""
+    resource = 'http://127.0.0.1:5000'
+    buy._buy(mock_config, resource, info_only=True)
+
+    assert not hasattr(patch_bitrequests, 'method')
+    assert not hasattr(patch_bitrequests, 'url')
+    assert not hasattr(patch_bitrequests, 'max_price')
+    assert not hasattr(patch_bitrequests, 'headers')
+    patch_click.assert_called_once_with('\n'.join(['{}: {}'.format(k, v) for k, v in mock.MockBitRequests.HEADERS.items()]))
+
+
+def test_post_url_buy(patch_click, mock_config, patch_bitrequests, patch_rest_client):
+    """Test a POST buy with form url-encoded data."""
+    resource = 'http://127.0.0.1:5000'
+    buy._buy(mock_config, resource, data='type=test')
+
+    assert patch_bitrequests.method == 'post'
+    assert patch_bitrequests.url == resource
+    assert patch_bitrequests.max_price == 10000
+    assert patch_bitrequests.headers['Content-Type'] == 'application/x-www-form-urlencoded'
+    assert patch_bitrequests.data['type'] == 'test'
+    assert patch_bitrequests.response.status_code == 201
+    assert patch_bitrequests.data['type'] in patch_bitrequests.response.text
+    assert patch_bitrequests.response.amount_paid == mock.MockBitResponse.POST_COST
+    assert patch_click.call_count == 2
+    patch_click.assert_any_call('{"type": "test"}', file=None)
+    patch_click.assert_any_call(uxstring.UxString.buy_balances.format(patch_bitrequests.response.amount_paid, '21.co', mock.MockTwo1Wallet.BALANCE - mock.MockBitResponse.POST_COST), err=True)
+
+
+def test_post_json_buy(patch_click, mock_config, patch_bitrequests, patch_rest_client):
+    """Test a POST buy with json-encoded data."""
+    resource = 'http://127.0.0.1:5000'
+    buy._buy(mock_config, resource, data='{"type": "test"}')
+
+    assert patch_bitrequests.method == 'post'
+    assert patch_bitrequests.url == resource
+    assert patch_bitrequests.max_price == 10000
+    assert patch_bitrequests.headers['Content-Type'] == 'application/json'
+    assert patch_bitrequests.data['type'] == 'test'
+    assert patch_bitrequests.response.status_code == 201
+    assert patch_bitrequests.data['type'] in patch_bitrequests.response.text
+    assert patch_bitrequests.response.amount_paid == mock.MockBitResponse.POST_COST
+    assert patch_click.call_count == 2
+    patch_click.assert_any_call('{"type": "test"}', file=None)
+    patch_click.assert_any_call(uxstring.UxString.buy_balances.format(patch_bitrequests.response.amount_paid, '21.co', mock.MockTwo1Wallet.BALANCE - mock.MockBitResponse.POST_COST), err=True)
+
+def test_buy_headers(patch_click, mock_config, patch_bitrequests, patch_rest_client):
+    """Test a buy with custom headers."""
+    resource = 'http://127.0.0.1:5000'
+    buy._buy(mock_config, resource, header=('Authorization: Bearer MYTESTKEY',))
+
+    assert patch_bitrequests.method == 'get'
+    assert patch_bitrequests.url == resource
+    assert patch_bitrequests.max_price == 10000
+    assert patch_bitrequests.headers['Authorization'] == 'Bearer MYTESTKEY'
+    assert patch_bitrequests.data is None
+    assert patch_bitrequests.response.status_code == 200
+    assert patch_bitrequests.response.text == mock.MockBitResponse.SUCCESS_RESPONSE
+    assert patch_bitrequests.response.amount_paid == mock.MockBitResponse.GET_COST
+    assert patch_click.call_count == 2
+    patch_click.assert_any_call(mock.MockBitResponse.SUCCESS_RESPONSE, file=None)
+    patch_click.assert_any_call(uxstring.UxString.buy_balances.format(patch_bitrequests.response.amount_paid, '21.co', mock.MockTwo1Wallet.BALANCE - mock.MockBitResponse.GET_COST), err=True)
+
+
+def test_non_buy(patch_click, mock_config, patch_bitrequests, patch_rest_client):
+    """Test a buy that does not hit a 402-payable endpoint."""
+    resource = 'http://127.0.0.1:5000'
+    buy._buy(mock_config, resource, method='put')
+
+    assert patch_bitrequests.method == 'put'
+    assert patch_bitrequests.url == resource
+    assert patch_bitrequests.max_price == 10000
+    assert patch_bitrequests.data is None
+    assert patch_bitrequests.response.status_code == 405
+    assert patch_bitrequests.response.text == mock.MockBitResponse.FAILURE_RESPONSE
+    assert not hasattr(patch_bitrequests.response, 'amount_paid')
+    patch_click.assert_called_once_with(mock.MockBitResponse.FAILURE_RESPONSE, file=None)
+
+
+def test_error_buys(patch_click, mock_config, patch_bitrequests):
+    """Test buy failure under various error cases."""
+    resource = 'http://127.0.0.1:5000'
+
+    # Test disallowed payment method
+    with pytest.raises(click.ClickException) as e:
+        buy._buy(mock_config, resource, payment_method='fake')
+    assert str(e.value) == uxstring.UxString.buy_bad_payment_method.format('fake')
+
+    # Test improper resource URL
+    with pytest.raises(click.ClickException) as e:
+        buy._buy(mock_config, '127.0.0.1:5000')
+    assert str(e.value) == uxstring.UxString.buy_bad_uri_scheme
+
+    # Test improper resource URL
+    with pytest.raises(click.ClickException) as e:
+        buy._buy(mock_config, 'http://')
+    assert str(e.value) == uxstring.UxString.buy_bad_uri_host
+
+    assert patch_click.called is False
+
+
+def test_parse_post_data():
+    """Test utility functionality for parsing data."""
+    form_url = 'type=test&message=hey hey hey!'
+    json = '{"type": "test", "message": "hey hey hey!"}'
+    invalid = 'type: test; message: hey hey hey!'
+
+    data_dict, content_type = buy._parse_post_data(form_url)
+    assert isinstance(data_dict, dict)
+    assert data_dict['type'] == 'test'
+    assert data_dict['message'] == 'hey hey hey!'
+    assert content_type == 'application/x-www-form-urlencoded'
+
+    data_dict, content_type = buy._parse_post_data(json)
+    assert isinstance(data_dict, dict)
+    assert data_dict['type'] == 'test'
+    assert data_dict['message'] == 'hey hey hey!'
+    assert content_type == 'application/json'
+
+    with pytest.raises(click.ClickException) as e:
+        data_dict, content_type = buy._parse_post_data(invalid)
+    assert str(e.value) == uxstring.UxString.buy_bad_data_format
---
+++ two1/tests/mock.py
@@ -0,0 +1,157 @@
+"""Mock objects for testing."""
+import json
+import codecs
+import collections
+
+import two1.lib.bitcoin as bitcoin
+import two1.lib.channels as channels
+import two1.lib.server.machine_auth_wallet as machine_auth_wallet
+
+
+class MockTwo1Wallet:
+
+    """Mock Two1 Wallet."""
+
+    PRIVATE_KEY = bitcoin.PrivateKey.from_int(0x70b5d984b8a8e072e201ddd59ff3deb2d7303467136001c062ffa23552ea058e)
+    BALANCE = 123000
+
+    def __init__(self):
+        """Returns a new MockTwo1Wallet object."""
+        self._balance = MockTwo1Wallet.BALANCE
+
+    def sign_message(self, message, account_name_or_index=None, key_index=0):
+        """Sign an arbitrary message."""
+        return codecs.encode(bytes(self.PRIVATE_KEY.sign(message)), 'base64_codec').decode()
+
+    def get_message_signing_public_key(self):
+        """Return a mock public key for signing messages."""
+        return self.PRIVATE_KEY.public_key
+
+    def confirmed_balance(self):
+        """Return the wallet's confirmed balance in satoshis."""
+        return self.BALANCE
+
+    def unconfirmed_balance(self):
+        """Return the wallet's unconfirmed balance in satoshis."""
+        return self._balance
+
+    def pay(self, amount):
+        """Make a payment."""
+        self._balance = self._balance - amount
+
+
+class MockBitResponse:
+
+    """Mock response from a BitRequests request."""
+
+    SUCCESS_RESPONSE = 'You bought something with bitcoin!'
+    FAILURE_RESPONSE = 'Method not supported!'
+    GET_COST = 5000
+    POST_COST = 20000
+
+    def __init__(self, method='get', data=None, headers={}):
+        """Return a new MockBitResponse object."""
+        if method.upper() == 'GET':
+            self.status_code = 200
+            self.amount_paid = self.GET_COST
+            self.text = self.SUCCESS_RESPONSE
+        elif method.upper() == 'POST':
+            self.status_code = 201
+            self.amount_paid = self.POST_COST
+            self.text = json.dumps(data)
+        else:
+            self.status_code = 405
+            self.text = self.FAILURE_RESPONSE
+
+
+class MockBitRequests:
+
+    """Mock BitRequests behavior."""
+
+    HEADERS = {'Bitcoin-Address': '1ExZSz3NsfEqKXnFrg5cJ8F3UTRzcnZ8J7', 'Price': MockBitResponse.GET_COST}
+
+    def __init__(self, wallet, username=None):
+        """Return a new MockBitRequests object."""
+        self.wallet = wallet
+        self.username = username
+
+    def get_402_info(self, resource):
+        """Mock 402-related headers."""
+        return MockBitRequests.HEADERS
+
+    def request(self, method, url, max_price=None, **kwargs):
+        """Mock a paid 402 request."""
+        self.method = method
+        self.url = url
+        self.max_price = max_price
+        any(setattr(self, key, val) for key, val in kwargs.items())
+        self.response = MockBitResponse(self.method, self.data, self.headers)
+        if hasattr(self.wallet, 'pay') and hasattr(self.response, 'amount_paid'):
+            self.wallet.pay(self.response.amount_paid)
+        elif hasattr(self.response, 'amount_paid'):
+            self.wallet.wallet.pay(self.response.amount_paid)
+        return self.response
+
+
+class MockTwentyOneRestClient:
+
+    """Mock TwentyOneRestClient behavior."""
+
+    FLUSHED = 240000
+
+    def __init__(self, server_url, machine_auth, username=None):
+        """Return a new MockTwentyOneRestClient object."""
+        self.server_url = server_url
+        self.machine_auth = machine_auth
+        self.username = username
+        self._earnings = self.machine_auth.wallet.BALANCE
+        self._flushed = MockTwentyOneRestClient.FLUSHED
+
+    def get_earnings(self):
+        """Get net earnings."""
+        return dict(total_earnings=self.machine_auth.wallet.unconfirmed_balance(), flushed_amount=self._flushed)
+
+
+class MockChannelClient:
+
+    """Mock ChannelClient behavior."""
+
+    Channel = collections.namedtuple('Channel', ['balance', 'state'])
+    URL = 'test://fake'
+    BALANCE = 99000
+
+    def __init__(self):
+        """Return a new MockChannelClient object."""
+        self.channels = {}
+        self.channels[MockChannelClient.URL] = MockChannelClient.Channel(MockChannelClient.BALANCE, channels.PaymentChannelState.READY)
+
+    def sync(self):
+        """Mocks the channel `sync` method."""
+        pass
+
+    def list(self):
+        """Mocks the channel `list` method."""
+        return self.channels.keys()
+
+    def status(self, url):
+        """Mocks the channel `list` method."""
+        return self.channels[url]
+
+
+class MockConfig:
+
+    """Mock Config behavior."""
+
+    collect_analytics = False
+
+    def __init__(self):
+        """Return a new MockConfig object."""
+        self.wallet = MockTwo1Wallet()
+        self.machine_auth = machine_auth_wallet.MachineAuthWallet(self.wallet)
+        self.channel_client = MockChannelClient()
+        self.username = 'MockTestUser'
+        self.purchases = []
+
+    def log_purchase(self, **kwargs):
+        """Mock purchase log."""
+        self.purchases.append(kwargs)
