98ff90e38216e7358a67d98cbcdc0fc7b46cde9f
Kodie <kodie@21.co>
2016-02-19 17:44:41 -0800

    setup.py: upate packages with new utils path

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
               'two1.lib.mining', 'two1.lib.server', 'two1.lib.wallet',
               'two1.lib.crypto', 'two1.lib.bitserv', 'two1.lib.channels',
               'two1.lib.bitserv.django', 'two1.lib.bitserv.flask',
-              'two1.lib.blockchain', 'two1.lib.bitrequests', 'two1.lib.util',],
+              'two1.lib.blockchain', 'two1.lib.bitrequests', 'two1.commands.util',],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
