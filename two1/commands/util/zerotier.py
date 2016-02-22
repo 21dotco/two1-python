611803b5c3dd62f4f11682c63107de710033923f
Kodie <kodie@21.co>
2016-02-22 14:52:03 -0800

    commands/util/zerotier: improve zerotier api
    Added docstrings to all functions and added is_installed()
    and start_daemon() to make cross platform support cleaner.

--- two1/commands/util/zerotier.py
+++ two1/commands/util/zerotier.py
@@ -4,10 +4,21 @@ Simple wrapper for zerotier-cli
 """
 import json
 import subprocess
+import shutil
+import platform
+
+
+def is_installed():
+    """ Checks the system whether zerotier-one is installed
+
+    Returns:
+        bool: True if zerotier-one is installed, False otherwise
+    """
+    return shutil.which('zerotier-cli') is not None
 
 
 def cli(*args):
-    """ Runs zerotier-cli in sudo mode and returns the results
+    """ Runs zerotier-cli as superuser and returns the results
 
     Args:
         *args: List of string arguments to zerotier-cli
@@ -15,14 +26,14 @@ def cli(*args):
     Returns:
         str: A string with the entire output of the shell command.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
     """
     return subprocess.check_output(("sudo", "zerotier-cli") + args)
 
 
 def cli_json(*cmd):
-    """ Runs zerotier-cli in sudo mode and returns the results in json format
+    """ Runs zerotier-cli as superuser and returns the results in json format
 
     Args:
         *args: List of string arguments to zerotier-cli
@@ -30,7 +41,7 @@ def cli_json(*cmd):
     Returns:
         dict: A dict with the json parsed results of the command.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
     """
@@ -39,18 +50,27 @@ def cli_json(*cmd):
     return json.loads(text)
 
 
-def is_valid(idstr, idlen=16):
+def is_valid(id_str, id_len=16):
     """ Simple check for a valid zerotier network_id
 
     Args:
-        idstr (str): Zerotier network id
+        id_str (str): Zerotier network id or address
 
     Returns:
-        bool: True if the network_id is valid, False otherwise
+        bool: True if the id_str is valid, False otherwise
+
+    Raises:
+        ValueError: if the length of the id string is not equal to the expected id length
     """
-    assert len(idstr) == idlen
-    for char in idstr:
-        assert char in '0123456789abcdef'
+    if len(id_str) != id_len:
+        raise ValueError("Error: length of the id_str is not equal to the expected length")
+
+    try:
+        # expected to be valid hexadecmal string
+        int(id_str, 16)
+    except ValueError:
+        return False
+
     return True
 
 
@@ -60,7 +80,7 @@ def info():
     Returns:
         dict: A dict with the json parsed results of the command.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
     """
@@ -73,12 +93,12 @@ def device_address():
     Returns:
         str: Zerotier device id
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
     """
     address = info()['address']
-    assert is_valid(address, idlen=10)
+    assert is_valid(address, id_len=10)
     return address
 
 
@@ -88,7 +108,7 @@ def list_networks():
     Returns:
         list: A list of networks (dict) that the device is connected to.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
     """
@@ -97,6 +117,7 @@ def list_networks():
 
 def device_ip(network_id=None):
     """ Returns the IP address and network mask for the provided Zerotier network.
+
         If no network is provided, this will return the IP if the device is connected to
         only one Zerotier network.
 
@@ -105,6 +126,10 @@ def device_ip(network_id=None):
 
     Returns:
         list (IP, mask): Returns the IP and the mask. e.g. [u'172.23.15.14', u'16']
+
+    Raises:
+        NameError: if the network_id given is not a valid network id or an IP address
+            has not been assigned yet.
     """
     networks = list_networks()
     ret = None
@@ -128,7 +153,7 @@ def list_peers():
     Returns:
         list: A list of peers (dict) that the device can see.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
     """
@@ -144,13 +169,15 @@ def join_network(network_id):
     Returns:
         str: Result from the zerotier-cli join call.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
+        ValueError: If the given network_id is invalid.
     """
-    if is_valid(network_id, idlen=16):
-        result = cli('join', network_id)
-        return result
+    if not is_valid(network_id, id_len=16):
+        raise ValueError("Error network_id ({}) is not valid")
+
+    return cli('join', network_id)
 
 
 def leave_network(network_id):
@@ -162,24 +189,70 @@ def leave_network(network_id):
     Returns:
         str: Result from the zerotier-cli leave call.
 
-    Exceptions:
+    Raises:
         CalledProcessError: If the cli call failed.
         ValueError: If the json string could not be successfully parsed into a dict.
+        ValueError: If the given network_id is invalid.
     """
-    if is_valid(network_id, idlen=16):
-        result = cli('leave', network_id)
-        return result
+    if not is_valid(network_id, id_len=16):
+        raise ValueError("Error network_id ({}) is not valid")
+
+    return cli('leave', network_id)
 
 
-def get_address_for_network(network_name):
+def start_daemon():
+    """ Starts the zerotier daemon if it is installed on your system
+
+    Returns:
+       str: output of the subprocess call to check_output
+
+    Raises:
+        EnvironmentError: if you your ststem is not yet supported.
+        CalledProcessError: if the command to start the daemon failed.
+    """
+    if platform.system() in "Linux":
+        if shutil.which("systemctl"):
+            cmd = ('sudo', 'systemctl', 'start', 'zerotier-one.service')
+        elif shutil.which("service"):
+            cmd = ('sudo', 'service', 'zerotier-one', 'start')
+        else:
+            raise EnvironmentError("Do not know how to start zerotier deamon on your system")
+    elif platform.system() in "Darwin":
+        # ZT post install for Macs already load the daemon
+        return
+
+    return subprocess.check_output(cmd)
+
+def get_address(network_name):
+    """ Gets the IP address of the given network name
+
+    Returns:
+        str: an IP address in string format if the network given exists and has
+            an assigned address, None otherwise.
+    """
     all_addresses = get_all_addresses()
-    return all_addresses[network_name]
+    if network_name in all_addresses:
+        return all_addresses[network_name]
+
+    return None
 
 
 def get_all_addresses():
+    """ Gets all addresses in a dictionary format with the network names as keys
+
+    Returns:
+        dict: a dictionary of IP addresses with network names as keys, or an empty dict
+            if no networks are found.
+    """
     result = {}
-    for network in list_networks():
+    try:
+        networks = list_networks()
+    except (ValueError, CalledProcessError):
+        return result
+
+    for network in networks():
         address_and_mask = network["assignedAddresses"][0]
         address = address_and_mask.split("/")[0]
         result[network["name"]] = address
+
     return result
