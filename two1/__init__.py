b3b1b77549cd9042bfbe12f8ada42a10bf88e802
Kodie <kodie@21.co>
2016-02-26 16:08:08 -0800

    two1: load .env file on two1 import

--- two1/__init__.py
+++ two1/__init__.py
@@ -12,11 +12,33 @@ except ImportError:
     except:
         raise Exception('Version not found. Is there a tag available?')
 
-# Define all project global variables
+
+# Defines hard coded global variables
 TWO1_VERSION = version
 TWO1_PACKAGE_NAME = 'two1'
 TWO1_USER_FOLDER = os.path.expanduser('~/.two1/')
 TWO1_CONFIG_FILE = TWO1_USER_FOLDER + 'two1.json'
+# two parents up from current dir
+TWO1_BASE_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir, os.pardir))
+
+
+# simple logic to load the environment only once
+if "env_loaded" not in locals():
+    env_loaded = True
+
+    # ensures the file exists
+    dotenv_path = os.path.join(TWO1_BASE_DIR, ".env")
+    if os.path.exists(dotenv_path):
+        with open(dotenv_path, "rt") as f:
+            for line in f:
+                if not line or line.startswith('#') or '=' not in line:
+                    continue
+                key, value = line.strip().split('=', 1)
+                value = value.strip("'").strip('"')
+                os.environ.setdefault(key, value)
+
+
+# Defines configurable global variables
 TWO1_HOST = os.environ.get('TWO1_HOST', 'https://api.21.co')
 TWO1_PROVIDER_HOST = os.environ.get('TWO1_PROVIDER_HOST', 'https://blockchain.21.co')
 TWO1_PYPI_HOST = os.environ.get('TWO1_PYPI_HOST', 'https://pypi-3844.21.co')
