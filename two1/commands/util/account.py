510dd4ab987677fbeb1c58d0eb283823ce984894
Kodie <kodie@21.co>
2016-03-02 15:55:40 -0800

    commands/util/account: use login command logic
    for account creation and logging a user in

--- two1/commands/util/account.py
+++ two1/commands/util/account.py
@@ -1,23 +1,47 @@
 """Utility functions for user accounts."""
+# standard python imports
 import sys
 
+# 3rd party imports
+import click
+
+# two1 imports
+import two1
 from two1.commands.util import uxstring
-from two1.lib.server import login
-from two1.lib.blockchain import exceptions as bc_exceptions
-from two1.commands.util import exceptions as cmd_exceptions
+from two1.commands import login
 
 
-def get_or_create_username(config):
-    """Create a new wallet or return the currently existing one."""
-    try:
-        login.check_setup_twentyone_account(config)
-    except bc_exceptions.DataProviderUnavailableError:
-        raise cmd_exceptions.TwoOneError(uxstring.UxString.Error.connection_cli)
-    except bc_exceptions.DataProviderError:
-        raise cmd_exceptions.TwoOneError(uxstring.UxString.Error.server_err)
-    except cmd_exceptions.UnloggedException:
-        sys.exit(1)
+def get_or_create_username(config, machine_auth):
+    """ Gets an existing username or creates a new account
+
+        On a bitcoin computer a user can create one account per
+        machine auth wallet. When not on a BC a user must log into
+        an existing account created at the free signup page.
+
+    Args:
+        config (Config): config object used for getting .two1 information
+        machine_auth (MachineAuthWallet): machine auth wallet used for authentication
+
+    Returns:
+        str: username of the current user on the system
+    """
+    # User hasn't logged in with the wallet
+    if not config.mining_auth_pubkey:
+        # A user can create an account on a BC
+        if two1.TWO1_DEVICE_ID:
+            login.create_account_on_bc(config, machine_auth)
+
+        # log into an existing account
+        else:
+            login.login_account(config, machine_auth)
 
     if not config.username:
+        click.echo(uxstring.UxString.Error.login_error_username)
         sys.exit(1)
+
+    if not config.mining_auth_pubkey:
+        click.echo(uxstring.UxString.Error.login_error_mining_auth_pubkey)
+        sys.exit(2)
+
     return config.username
+
