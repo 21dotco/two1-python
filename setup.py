9b78a3a73eab3cb253e485da5040b2a81456c4f8
Kodie <kodie@21.co>
2016-03-15 14:16:55 -0700

    setup: remove mining package

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=['two1', 'two1.lib', 'two1.commands', 'two1.commands.helpers', 'two1.lib.bitcoin',
-              'two1.lib.mining', 'two1.lib.server', 'two1.lib.wallet',
+              'two1.lib.server', 'two1.lib.wallet',
               'two1.lib.crypto', 'two1.lib.bitserv', 'two1.lib.channels',
               'two1.lib.bitserv.django', 'two1.lib.bitserv.flask',
               'two1.lib.blockchain', 'two1.lib.bitrequests', 'two1.commands.util',],
