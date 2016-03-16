7170552082459317f50dc5c9a465fa70de4010e8
Tyler J <tylerj@21.co>
2016-03-15 19:07:01 -0700

    lib/bitrequests/__init__: add generic bitrequests usage

--- two1/lib/bitrequests/__init__.py
+++ two1/lib/bitrequests/__init__.py
@@ -10,3 +10,43 @@ from .bitrequests import ChannelRequests
 from .bitrequests import BitRequestsError
 from .bitrequests import UnsupportedPaymentMethodError
 from .bitrequests import ResourcePriceGreaterThanMaxPriceError
+
+_requests = None
+
+
+def request(*args, payment_method='offchain', **kwargs):
+    global _requests
+
+    if _requests is None:
+        from two1.lib.wallet import Wallet
+        if payment_method == 'offchain':
+            from two1.lib.server.machine_auth_wallet import MachineAuthWallet
+            _requests = BitTransferRequests(MachineAuthWallet(Wallet()))
+        elif payment_method == 'onchain':
+            _requests = OnChainRequests(Wallet())
+        elif payment_method == 'channel':
+            _requests = ChannelRequests(Wallet())
+        else:
+            raise ValueError('That method is not supported.')
+
+    return _requests.request(*args, **kwargs)
+
+
+def get(*args, **kwargs):
+    return request('get', *args, **kwargs)
+
+
+def put(*args, **kwargs):
+    return request('put', *args, **kwargs)
+
+
+def post(*args, **kwargs):
+    return request('post', *args, **kwargs)
+
+
+def delete(*args, **kwargs):
+    return request('delete', *args, **kwargs)
+
+
+def head(*args, **kwargs):
+    return request('head', *args, **kwargs)
