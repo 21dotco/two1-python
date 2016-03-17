13e00b60eb9bd8c6bc4e3822f10be8fb4a2c61eb
Kodie <kodie@21.co>
2016-03-16 17:47:26 -0700

    commands/util/decorators: use log levels for json_output decorator

--- two1/commands/util/decorators.py
+++ two1/commands/util/decorators.py
@@ -29,35 +29,60 @@ def docstring_parameter(*args, **kwargs):
 
 
 def json_output(f):
-    """Allows the return value to be optionally returned as json output
-       with the '--json' flag."""
-    @click.option('--json',
-              default=False,
-              is_flag=True,
-              help='Uses JSON output.')
-    @click.pass_context
-    def wrapper(ctx, json, *args, **kwargs):
+    """ Allows the return value to be optionally returned as json output with the '--json' flag.
 
-        # protect against early cli failures
-        if ctx.obj and 'config' in ctx.obj:
-            config = ctx.obj['config']
-            config.set_json_output(json)
+        When a command fails but still has data to be printed as json, a Two1Error should be
+        raised because it takes an optional json param. This means we can pass any json data
+        using the Two1Error as a vehicle to "return" the json data to this decorator to allow
+        printing. This design was motivated by the `21 doctor` command because when a doctor
+        check fails it raises an exception and passes the results using the exception.
+
+        This could be changed to to have the return value expect a key of "error" in the case
+        of an error but since two1 uses exceptions to halt execution this design makes sense.
+    """
+
+    @click.option('--json', default=False, is_flag=True, help='Uses JSON output.')
+    @click.pass_context
+    def _json_output(ctx, json, *args, **kwargs):
+        """ This wrapper disables logging when json is set and restores it after print json value
+
+            In order for this to work ALL output printed to console needs to be done through
+            a logger.
+        """
+        # call early if --json wasn't given as a cmd line arg
+        if not json:
+            return f(ctx, *args, **kwargs)
+
+        # gets the original level to the decorator can restore it
+        original_level = logging.getLogger('two1').getEffectiveLevel()
+
+        # disables ALL log messages critical and below
+        logging.disable(logging.CRITICAL)
 
         try:
             result = f(ctx, *args, **kwargs)
-        except exceptions.Two1Error as e:
-            if (json):
-                err_json = e._json
-                err_json["error"] = e._msg
-                logger.info(jsonlib.dumps(err_json, indent=4, separators=(',', ': ')))
-            raise e
+        except exceptions.Two1Error as ex:
+            # sets the level back to original
+            logging.disable(original_level)
+
+            err_json = ex._json
+            err_json["error"] = ex._msg
+
+            # dumps the json error
+            logger.info(jsonlib.dumps(err_json, indent=4, separators=(',', ': ')))
+
+            raise ex
         else:
-            if (json):
-                logger.info(jsonlib.dumps(result, indent=4, separators=(',', ': ')))
+            # sets the level back to original
+            logging.disable(original_level)
+
+            # dumps the json result
+            logger.info(jsonlib.dumps(result, indent=4, separators=(',', ': ')))
+
 
         return result
 
-    return functools.update_wrapper(wrapper, f)
+    return functools.update_wrapper(_json_output, f)
 
 
 def check_notifications(func):
@@ -178,7 +203,12 @@ def catch_all(func):
 
         # raise all click exceptions because they are used to bail and print a message
         except click.ClickException:
-            raise
+            # dont raise exception if --json was given so no error messages are printed
+            # errors are printed in a json format in the json_decorator above
+            if "json" in ctx.params and ctx.params['json']:
+                return
+            else:
+                raise
 
         except Exception:
             # generic error string
