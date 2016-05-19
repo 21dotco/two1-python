dce47ec76ac740a60a8d0ed13924231e5e2a1316
Michael Goldstein <mg@21.co>
2016-05-18 18:18:25 -0700

    market: create nested commands

--- two1/commands/market.py
+++ two1/commands/market.py
@@ -13,43 +13,59 @@ from two1.commands.join import show_network_status
 from two1.commands.join import _join
 from two1.commands.join import _leave
 
-
-TWO1_MARKET = '21market'
-
 # Creates a ClickLogger
 logger = logging.getLogger(__name__)
 
 
-@click.command()
-@click.option('--join', is_flag=True, default=False,
-              help="Join 21's peer-to-peer network.")
-@click.option('--leave', is_flag=True, default=False,
-              help="Leave 21's peer-to-peer network.")
-@click.option('--status', is_flag=True, default=False,
-              help='Show status of all networks that you have joined.')
+@click.group(invoke_without_command=True)
 @click.pass_context
-@decorators.catch_all
-@decorators.capture_usage
-@decorators.check_notifications
-def market(ctx, join, leave, status):
-    """Join the peer-to-peer 21market network to buy/sell for BTC.
+def market(ctx):
+    """Join a p2p network to buy/sell for BTC.
 
 \b
 Usage
 -----
-21 market --join     # Join 21's peer-to-peer network
-21 market --leave    # Leave 21's peer-to-peer network
-21 market --status   # Confirm that you have joined
+21 market join     # Join 21's peer-to-peer network
+21 market leave    # Leave 21's peer-to-peer network
+21 market status   # Confirm that you have joined
 """
-    if status:
-        show_network_status()
-    elif leave:
-        _leave(ctx.obj['client'], TWO1_MARKET)
+    if ctx.invoked_subcommand is None:
+        logger.info(ctx.command.get_help(ctx))
+
+
+@market.command()
+@click.pass_context
+@decorators.catch_all
+@decorators.capture_usage
+@decorators.check_notifications
+def status(ctx):
+    """View the status of joined networks."""
+    show_network_status()
+
+
+@market.command()
+@click.argument("network", default="21market")
+@click.pass_context
+@decorators.catch_all
+@decorators.capture_usage
+@decorators.check_notifications
+def join(ctx, network):
+    """Join 21's p2p network."""
+    logger.info(uxstring.UxString.join_network_beta_warning)
+    response = getinput("I understand and wish to continue [y/n]: ", ["y", "n"])
+    if response == "y":
+        logger.info(uxstring.UxString.superuser_password)
+        _join(ctx.obj['client'], network)
     else:
-        logger.info(uxstring.UxString.join_network_beta_warning)
-        response = getinput("I understand and wish to continue [y/n]: ", ["y", "n"])
-        if response == "y":
-            logger.info(uxstring.UxString.superuser_password)
-            _join(ctx.obj['client'], TWO1_MARKET)
-        else:
-            logger.info(uxstring.UxString.join_network_beta_exit)
+        logger.info(uxstring.UxString.join_network_beta_exit)
+
+
+@market.command()
+@click.argument("network", default="21market")
+@click.pass_context
+@decorators.catch_all
+@decorators.capture_usage
+@decorators.check_notifications
+def leave(ctx, network):
+    """Leave 21's p2p network."""
+    _leave(ctx.obj['client'], network)
