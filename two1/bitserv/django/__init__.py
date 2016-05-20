a9e2955dc634e85ad8d7bfaf90ec8674abf12e30
Jeremy Kun <jeremy@21.co>
2016-05-20 12:09:37 -0700

    bitserv: Useful msg for django dependencies

--- two1/bitserv/django/__init__.py
+++ two1/bitserv/django/__init__.py
@@ -1,4 +1,17 @@
 """Bitserv implementation for Django."""
+try:
+    import django
+    import rest_framework
+except ImportError:
+    raise ImportError(
+        '''21 Django integration requires the following packages:
+
+django~=1.8.0
+djangorestframework==3.2.3
+
+Please install them with pip before continuing.'''
+    )
+
 from django.conf import settings
 from .decorator import Payment
 
