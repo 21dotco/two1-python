3d5b50a89efab6c5fb9b2c1684485017b999bdec
Jian Li <jian@21.co>
2016-08-24 18:48:49 -0700

    cli: Print errors to stderr

--- two1/commands/util/decorators.py
+++ two1/commands/util/decorators.py
@@ -58,7 +58,7 @@ def json_output(f):
             # sets the level back to original
             logging.disable(original_level)
 
-            err_json = ex._json
+            err_json = ex._json or {}
             err_json["error"] = ex._msg
 
             # dumps the json error
@@ -207,23 +207,25 @@ def capture_usage(func):
 
 
 def catch_all(func):
-    """ Adds a safety net to functions that catches all exceptions
+    """
+    Catch (nearly) all exceptions unless in debug mode.
 
     Args:
         func (function): function being decorated
     """
     def _catch_all(ctx, *args, **kwargs):
-        """ Catches all exceptions and prints the stacktrace if an environment variable is set
-
+        """
         Args:
             ctx (click.Context): cli context object
             args (tuple): tuple of args of the fuction
             kwargs (dict): keyword args of the function
         """
+        def stderr(msg):
+            click.echo(click.style(msg, fg='red'), file=sys.stderr)
         try:
             return func(ctx, *args, **kwargs)
         except click.Abort:
-            # on SIGINT click.prompt raise click.Abort
+            # on SIGINT click.prompt raises click.Abort
             logger.error('')  # just to get a newline
 
         except click.ClickException:
@@ -231,19 +233,19 @@ def catch_all(func):
 
         except Exception:
             # generic error string
-            logger.error(click.style(
-                'You have experienced a client-side technical error.', fg='red'))
+            stderr(
+                'You have experienced a client-side technical error.')
 
             # only dump the stack traces if the debug flag is set
             if kwargs.get('debug') or ctx.obj['debug']:
-                logger.error("\nFunction: {}.{}".format(func.__module__, func.__name__), fg="red")
-                logger.error("Args: {}".format(args), fg="red")
-                logger.error("Kwargs: {}".format(kwargs), fg="red")
-                logger.error("{}".format(traceback.format_exc()), fg="red")
+                stderr("\nFunction: {}.{}".format(func.__module__, func.__name__))
+                stderr("Args: {}".format(args))
+                stderr("Kwargs: {}".format(kwargs))
+                stderr("{}".format(traceback.format_exc()))
             else:
-                logger.error(click.style(
+                stderr(
                     'For more detail, run your command with the debug flag: '
-                    '`21 --debug <command>.`', fg='red'))
+                    '`21 --debug <command>.`')
         sys.exit(1)
 
     return functools.update_wrapper(_catch_all, func)
